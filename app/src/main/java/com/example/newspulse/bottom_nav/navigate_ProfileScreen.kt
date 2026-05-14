d0ab460df43e272159a36eddc896e44280a518f9
Anusha G M <anusha.gm-consultant@newscorp.com>
2026-05-14 10:13:19 +0530

    feat: implement Featured Stories LazyRow and UI layout in HomeScreen

---
+++ app/src/main/java/com/example/newspulse/bottom_nav/navigate_ProfileScreen.kt
@@ -0,0 +1,12 @@
+package com.example.newspulse.bottom_nav
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun profileScreenUI(){
+
+
+}
